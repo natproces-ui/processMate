db5ef335166b1429e4a1bc41d774acaae135c1fe
recruit-aiDev <developpeurrecruitai@gmail.com>
2025-11-19 11:55:42 +0100

    open api

--- frontend/src/app/api/ref.js
+++
@@ -1,9 +0,0 @@
-// pages/api/refs.js
-import { refs } from '../../data/ref.json'; // ou fetch depuis FastAPI
-
-export default function handler(req, res) {
-    res.status(200).json({
-        villes: refs.referentiels.villes,
-        nationalitePaysNaissance: refs.referentiels.nationalitePaysNaissance
-    });
-}
